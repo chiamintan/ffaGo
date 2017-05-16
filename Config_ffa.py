f4c2b03a6c2bb02543a185b62a36a8294e7088b6
emilieparent <parente@physics.mcgill.ca>
2017-05-16 11:58:30 -0400

    Update Config_ffa.py

--- Config_ffa.py
+++ Config_ffa.py
@@ -1,13 +1,10 @@
-
 import numpy as np
-
 #configuration file FFA
 
 #[FFA_settings]
-p_ranges = np.array([[0.1,0.5],[0.5,1.],[1.,2.],[2.,5.],[5.,10.],[10.,15.],[15,30]])
-dt_list = np.array([0.002,0.005,0.01,0.02,0.050,0.075,0.120])
-SN_tresh = 5
-
-# min_dc = 0.5, 1 or 1.5 
-mindc = 0.5
-numdms = 2
+p_ranges = np.array([[0.1,0.5],[0.5,1.],[1.,2.],[2.,5.],[5.,10.],[10.,15.]])
+dt_list = np.array([0.002,0.005,0.01,0.02,0.050,0.075])
+win_detrend = p_ranges[-1][-1]*2.
+SN_tresh    = 6.
+mindc 	    = 0.5     	# min_dc = 0.5 , 1 or 1.5 (this is the min duty-cycle in %)
+numdms      = 2
