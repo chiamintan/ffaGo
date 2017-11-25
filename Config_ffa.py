a6a540010a2b2cb63ac8a137833d7df25bd149e3
emilieparent <parente@physics.mcgill.ca>
2017-11-25 14:36:23 -0500

    Update Config_ffa.py

--- Config_ffa.py
+++ Config_ffa.py
@@ -8,4 +8,4 @@ win_detrend = p_ranges[-1][-1]*2. # twice the largest trial period
 SN_tresh    = 6.
 mindc 	    = 0.5     	# min_dc = 0.5 , 1 or 1.5 (this is the min duty-cycle in %)
 numdms      = 2         # relevant for sifting with ffa_final.py 
-metric      = 'A'       # 'A' : SNR = max-med/std, or 'B': same but exluding a 20% window around the peak
+metric      = 'A'       # 'A', 'B', or 'C', see Parent et al. 2017 for details on the metrics.
