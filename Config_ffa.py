544e147bb841fe3a48187f8a5d7bdbff76cee1c8
emilieparent <parente@physics.mcgill.ca>
2018-01-29 12:05:37 -0500

    Update Config_ffa.py

--- Config_ffa.py
+++ Config_ffa.py
@@ -8,4 +8,4 @@ win_detrend = p_ranges[-1][-1]*2. # twice the largest trial period
 SN_tresh    = 6.
 mindc 	    = 0.5     	# min_dc = 0.5 , 1 or 1.5 (this is the min duty-cycle in %)
 numdms      = 2         # relevant for sifting with ffa_final.py 
-metric      = 'A'       # 'A', 'B', or 'C', see Parent et al. 2017 for details on the metrics.
+metric      = 'A'       # 'A', 'B', or 'C', see Parent et al. 2018 for details on the metrics.
